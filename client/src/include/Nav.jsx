7c0c1f7eba346b9a5450d1c8ef3f8ca7eb4cff19
yujuhye <juhye990521@naver.com>
2024-04-30 15:14:13 +0900

    Nav

--- client/src/include/Nav.jsx
+++ client/src/include/Nav.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/friend/friendList">FriendList</Link> &nbsp;
         </nav>
     );
 };
